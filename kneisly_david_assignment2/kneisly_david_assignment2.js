a9cf9f486805c27dfe68ed0cd8cb5e61172a95c9
Tyler Kneisly <tkneisly@fullsail.edu>
2012-03-04 17:09:41 -0500

    Declared initial variables.  Outlined functions.

--- kneisly_david_assignment2/kneisly_david_assignment2.js
+++ kneisly_david_assignment2/kneisly_david_assignment2.js
@@ -4,21 +4,33 @@
 // Time Travel
 
 
-//Declare Variables
+//Declare
+var	myYear = 1985,
+	myLocation = "Hill Valley",
+	yearsTraveled = 50,
+	timeMachine = "Delorean",
+	timeMachineSystems = ["Time circuits on.","Flux Capacitor...fluxxing.","Engine running."],
+	timeMachineVersion = [1985,2005,1885]
+	;
 
 
 //Initial Output
 
 
 //PROCEDURE
-
-//NUMBER
+console.log("Procedure:  Travel to some other time");
 
 //BOOLEAN
+console.log("Boolean:  Don't mess up the timeline");
 
-//ARRAY
+//NUMBER
+console.log("Number:  Fix the timeline that I messed up");
 
 //STRING
+console.log("String:  Fix the time machine, because it's a Delorean");
+
+//ARRAY
+console.log("Array:  Get home to my own timeline");
 
 
 //OUTPUT
