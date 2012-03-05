3da52178dead8445a1f35847727ff7134841f26d
Tyler Kneisly <tkneisly@fullsail.edu>
2012-03-04 19:39:02 -0500

    Progress on Procedure and Boolean. Comments provided for follow-up.

--- kneisly_david_assignment2/kneisly_david_assignment2.js
+++ kneisly_david_assignment2/kneisly_david_assignment2.js
@@ -7,10 +7,11 @@
 //Declare
 var	myYear = 1985,
 	myLocation = "Hill Valley",
-	yearsTraveled = 50,
+	yearsTraveled = "", //review later--should have date difference returned
 	timeMachine = "Delorean",
 	timeMachineSystems = ["Time circuits on.","Flux Capacitor...fluxxing.","Engine running."],
-	timeMachineVersion = [1985,2005,1885]
+	timeMachineVersion = [1985,2015,1955,1885]
+	timeParadox = "" //review later--might return "true/false"
 	;
 
 
@@ -20,8 +21,21 @@ var	myYear = 1985,
 //PROCEDURE
 console.log("Procedure:  Travel to some other time");
 
+var timeTravel = function(time) {
+	if (yearsTraveled != 0) {
+		console.log("I managed to get my " + timeMachine + " to work!");
+		
+		console.log("I've travelled " + yearsTraveled + " years from " + myYear + " to the year " + timeline + ".");
+	} else {
+		console.log("This " + timeMachine + " is a piece of garbage!");
+	}
+}; //timeTravel
+
 //BOOLEAN
-console.log("Boolean:  Don't mess up the timeline");
+console.log("Boolean:  Don't mess up the timeline"); //remove later
+var timeline = 2015; //remove later
+yearsTraveled = 30; //remove later
+timeTravel(yearsTraveled); //remove later
 
 //NUMBER
 console.log("Number:  Fix the timeline that I messed up");
