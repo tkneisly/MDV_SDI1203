a1ffba3d03895556d0d6bfc75baa567e3a04412f
Tyler Kneisly <tkneisly@fullsail.edu>
2012-03-05 00:21:46 -0500

    Update to last commit.

--- kneisly_david_assignment2/kneisly_david_assignment2.js
+++ kneisly_david_assignment2/kneisly_david_assignment2.js
@@ -11,7 +11,7 @@ var	myYear = 1985,
 	timeMachine = "Delorean",
 	timeMachineSystems = ["Time circuits on.","Flux Capacitor...fluxxing.","Engine running."],
 	timeMachineVersion = [1985,2015,1955,1885]
-	timeParadox = "" //review later--might return "true/false"
+	/*timeParadox = "" //review later--might return "true/false"*/
 	;
 
 
@@ -20,21 +20,37 @@ var	myYear = 1985,
 
 //PROCEDURE
 //Travel to some other time
-
 var timeTravel = function(time) {
 	if (yearsTraveled != 0) {
-		console.log("I managed to get my " + timeMachine + " to work!");
+		console.log("Only 88 mph and 1.21 gigawatts later, and I've violated the laws of physics!");
 		console.log("I've travelled " + yearsTraveled + " years from " + myYear + " to the year " + timeline + ".");
+		if (timeline < myYear) {
+			console.log("I've travelled through time to the past!");
+			console.log(" ");
+		} else {
+			console.log("I've travelled through time to the future!");
+			console.log(" ");
+		}
 	} else {
-		console.log("Well, it's a " + timeMachine + ", so of course I didn't go anywhere.");
+		console.log("Well, it's a " + timeMachine + ", so of course it didn't go anywhere.");
+		console.log(" ");
 	}
 }; //timeTravel
 
-var timeline = 2015; //remove later
-yearsTraveled = 0; //remove later
+var 	timeline = 1955, //remove later
+	paradox = "Mom and dad never meet, and I never...ex...ist...ed...",
+	yearsTraveled = 30; //remove later
 
 //BOOLEAN
 //Don't mess up the timeline
+var avoidParadoxes = function(year, paradox) {
+	var parentsNeverMeet = "",
+	westernRomance = "",
+	
+	if (year = 1955) {
+
+	}
+}
 
 //NUMBER
 //Fix the timeline that I messed up
@@ -47,6 +63,7 @@ yearsTraveled = 0; //remove later
 
 //OUTPUT
 timeTravel(yearsTraveled); //move later
+avoidParadox(timeline,paradox);
 
 //Procedure Output
 
