04dd2bc403d1b75c126cbcaecd9b615365406c7d
Tyler Kneisly <tkneisly@fullsail.edu>
2012-03-11 22:58:47 -0400

    Flowchart elements added to JS file outline.

--- kneisly_david_assignment3/kneisly_david_assignment3.js
+++ kneisly_david_assignment3/kneisly_david_assignment3.js
@@ -1,1 +1,76 @@
-alert("JavaScript works!");
+// SDI 1203
+// Week 3 Activity: Project 3
+// David Tyler Kneisly
+// Time Travel Objects
+
+
+// TERMINATORS
+// Start
+// Finish
+
+
+// CONNECTOR
+// Back
+
+
+// INTERNAL STORAGE
+// Global Variables
+
+// Local Variables
+
+// Returned Values
+
+// Property:  Boolean
+
+// Property:  Array
+
+// Property:  Number
+
+// Property:  String
+
+// Property:  Object
+
+
+
+// PROCESS
+// Nested Loop
+
+// While Loop
+
+// For Loop
+
+// Math
+
+
+
+// PREDEFINED PROCESS
+// Method:  Procedure
+
+// Method:  Function
+
+// Method:  Accessor
+
+// Method:  Mutator
+
+
+
+// DOCUMENT
+// JSON Data
+
+
+
+// DATA
+// Argument:  Boolean
+
+// Argument:  Array
+
+// Argument:  Number
+
+// Argumnet:  String
+
+// Argument:  Object
+
+
+
+// DISPLAY
+// Output
