ea9834a669dafeb222d38f742ce65a1a6383d280
Tyler Kneisly <tkneisly@fullsail.edu>
2012-03-22 22:20:23 -0400

    Final commit for Week 4, Assignment 4

--- kneisly_david_assignment4/kneisly_david_assignment4.js
+++ kneisly_david_assignment4/kneisly_david_assignment4.js
@@ -10,6 +10,7 @@ var emailAddress = "mostamazingpersonever@gmail.com";
 var website = "https://www.facebook.com";
 var movieTitle = "back to the future";
 var jobs = "Programer, Analyst, Consultant, Secret Agent";
+var gigawatts = 1.212934;
 
 
 //STRING
@@ -54,11 +55,11 @@ var checkURL = function(url) {
 };
 
 //Title-case a String
-var makeTitle = function(string) {
+var makeTitle = function(title) {
    cnvrt = function() {
       return arguments[0].toUpperCase();
    }
-   newTitle = string.toLowerCase().replace(/\b[a-z]/g, cnvrt);
+   newTitle = title.toLowerCase().replace(/\b[a-z]/g, cnvrt);
    return newTitle;
 };
 
@@ -66,6 +67,14 @@ var makeTitle = function(string) {
 var changeSeparator = function(jobString) {
    jobList = jobString.replace(/\,/g," /");
    return jobList;
+};
+
+
+//NUMBERS
+//Set Decimal Places
+var setDecimal = function(floatNum) {
+   newNumber = floatNum.toFixed(2);
+   return newNumber;
 };
 
 
@@ -82,28 +91,14 @@ console.log(emailCheck);
 checkURL(website);
 console.log(urlCheck);
 
-//Title-case
+//String: Title-case
 makeTitle(movieTitle);
 console.log(newTitle);
 
-//Convert Separators
+//String: Convert Separators
 changeSeparator(jobs);
 console.log(jobList);
 
-
-/*
-
-STRINGS
-   * Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
-
-NUMBERS
-   * Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
-   * Fuzzy-match a number: is the number above or below a number within a certain percent?
-   * Find the number of hours or days difference between two dates.
-   * Given a string version of a number such as "42", return the value as an actual Number, such as 42.
-
-ARRAYS
-   * Find the smallest value in an array that is greater than a given number
-   * Find the total value of just the numbers in an array, even if some of the items are not numbers.
-   * Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
-*/
+//Number: Set Decimal Places
+setDecimal(gigawatts);
+console.log(newNumber);
